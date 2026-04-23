94957e5b38869c0a03f3b7481a162a904ae4a5f6
ljl <2987163443a@gmail.com>
2026-04-23 12:21:02 +0800

    fix: ios 静音模式播放没有声音，obeyMuteSwitch在全局声明

--- src/app.tsx
+++ src/app.tsx
@@ -4,9 +4,12 @@ import { login } from '@/api/auth'
 import { getToken } from '@/api/token'
 import { resolveLogin, rejectLogin } from '@/utils/loginReady'
 import './styles/app.scss'
+/* eslint-disable no-undef */
+declare const wx: any
 
 class App extends Component {
   componentDidMount() {
+    wx.setInnerAudioOption({ obeyMuteSwitch: false })
     this.initLogin()
   }
 
